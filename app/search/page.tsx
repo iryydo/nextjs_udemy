3b2bf42d33f08830829ea66d6d08c9c2cd8e398d
Fudeel <fadil_mucia@hotmail.com>
2023-09-14 11:37:38 +0200

    fetch rest by qp

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,7 +3,38 @@ import Navbar from '../components/Navbar'
 import Header from './components/Header'
 import SearchSideBar from './components/SearchSideBar'
 import RestaurantCard from './components/RestaurantCard'
-export default function Search() {
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
+
+const fetchRestaurantsByCity = async function (city: string | undefined) {
+    const select = {
+        id: true,
+        name: true,
+        main_image: true,
+        price: true,
+        cuisine: true,
+        location: true,
+        slug: true
+    }
+
+    if (!city) return prisma.restaurant.findMany({ select });
+    console.log("city", city)
+    return prisma.restaurant.findMany({
+        where: {
+            location: {
+                name: {
+                    equals: city.toLowerCase()
+                }
+            }
+        },
+        select
+    })
+}
+
+export default async function Search({ searchParams }: { searchParams: { city: string } }) {
+    const restaurants = await fetchRestaurantsByCity(searchParams.city)
+    console.log(searchParams.city, restaurants)
     return (
         <main>
             <Navbar />
@@ -11,7 +42,7 @@ export default function Search() {
             <div className="flex py-4 m-auto w-2/3 justify-between items-start">
                 <SearchSideBar />
                 <div className="w-5/6">
-                    <RestaurantCard />
+                    {restaurants.length ? <RestaurantCard /> : <p>Sorry, no restaurants found in the area.</p>}
                 </div>
             </div>
         </main>
