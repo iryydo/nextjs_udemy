bd0b2a51474a82a9de1ead7063d52f150e77cd84
Irina Dochia <irina.f.dochia@accenture.com>
2023-09-14 23:21:54 +0200

    populated restaurant card

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,49 +2,54 @@ import Navbar from '../components/Navbar'
 import Header from './components/Header'
 import SearchSideBar from './components/SearchSideBar'
 import RestaurantCard from './components/RestaurantCard'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient} from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 const fetchRestaurantsByCity = async function (city: string | undefined) {
-    const select = {
-        id: true,
-        name: true,
-        main_image: true,
-        price: true,
-        cuisine: true,
-        location: true,
-        slug: true
-    }
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true
+  }
 
-    if (!city) return prisma.restaurant.findMany({ select })
+  if (!city) return prisma.restaurant.findMany({ select })
 
-    return prisma.restaurant.findMany({
-        where: {
-            location: {
-                name: {
-                    equals: city.toLowerCase()
-                }
-            }
-        },
-        select
-    })
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city.toLowerCase()
+        }
+      }
+    },
+    select
+  })
 }
 
-export default async function Search({ searchParams }: { searchParams: { city: string } }) {
-    const restaurants = await fetchRestaurantsByCity(searchParams.city)
-    console.log("searchParams.city: ", searchParams.city, restaurants)
-    return (
-        <main>
-            <Navbar />
-            <Header />
-            <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-                <SearchSideBar />
-                <div className="w-5/6">
-                    {restaurants.length ? <RestaurantCard /> : <p>Sorry, no restaurants found in the area.</p>}
-                </div>
-            </div>
-        </main>
-
-    )
+export default async function Search ({searchParams}:{searchParams: { city: string }}) {
+    
+  const restaurants = await fetchRestaurantsByCity(searchParams.city)
+  return (
+    <main>
+      <Navbar />
+      <Header />
+      <div className='flex py-4 m-auto w-2/3 justify-between items-start'>
+        <SearchSideBar />
+        <div className='w-5/6'>
+          {restaurants.length ? (
+            restaurants.map(restaurant => (
+              <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+            ))
+          ) : (
+            <p>Sorry, no restaurants found in the area.</p>
+          )}
+        </div>
+      </div>
+    </main>
+  )
 }
